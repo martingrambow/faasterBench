05105f8324e2c1c746a5754c30fbef7e8f3e6d5a
Martin Grambow <grambow@tu-berlin.de>
2023-08-29 11:13:09 +0200

    new wrapper idea using iterations

--- wrapper/templates/google/template.js
+++ wrapper/templates/google/template.js
@@ -1,51 +1,75 @@
-
 const functions = require('@google-cloud/functions-framework');
 const escapeHtml = require('escape-html');
 
+function getRandomBool() {
+    return Math.floor(Math.random() * 2) !== 0;
+}
+
 functions.http('wrapperTest', (req, res) => {
-    var mode = escapeHtml(req.query.mode || req.body.mode || 'default (E)');
-	var experimentID = process.env.EXPERIMENTID;
-    switch(mode){
-        case "A":
-            start1 = Date.now();
-            function1()
-            end1 = Date.now();
+    var mode = escapeHtml(req.query.mode || req.body.mode || 'default (A)');
+    var iterations = escapeHtml(req.query.iterations || req.body.iterations || 5);
+    var experimentID = process.env.EXPERIMENTID;
+    let fun1 = [];
+    let fun2 = [];
 
-            start2 = Date.now();
-            function2()
-            end2 = Date.now();
+    for (let i = 0; i < iterations; i++) {
+        switch (mode) {
+            case "A":
+                if (getRandomBool) {
+                    start1 = Date.now();
+                    function1();
+                    end1 = Date.now();
 
-            console.log("faaster_" + experimentID + ": modeA, f1 " + (end1-start1) + "ms f2 " + (end2-start2) + "ms");
-            break;
-        case "B":
-            start2 = Date.now();
-            function2()
-            end2 = Date.now();
+                    start2 = Date.now();
+                    function2();
+                    end2 = Date.now();
+                } else {
+                    start2 = Date.now();
+                    function2();
+                    end2 = Date.now();
 
-            start1 = Date.now();
-            function1()
-            end1 = Date.now();
+                    start1 = Date.now();
+                    function1();
+                    end1 = Date.now();
+                }
 
-            console.log("faaster_" + experimentID + ": modeB, f1 " + (end1-start1) + "ms f2 " + (end2-start2) + "ms");
-            break;
-        case "C":
-            start1 = Date.now();
-            function1()
-            end1 = Date.now();
+                fun1.push((end1 - start1));
+                fun2.push((end2 - start2));
+                break;
+            case "B":
+                start1 = Date.now();
+                function1();
+                end1 = Date.now();
 
-            console.log("faaster_" + experimentID + ": mode C, f1 " + (end1-start1) + "ms");
-            break;
-        case "D":
-            start2 = Date.now();
-            function2()
-            end2 = Date.now();
+                fun1.push((end1 - start1));
+                break;
+            case "C":
+                start2 = Date.now();
+                function2();
+                end2 = Date.now();
 
-            console.log("faaster_" + experimentID + ": mode D, f2 " + (end2-start2) + "ms");
-            break;
-        default:
-            //threaded
-            //web worker approach? needs separate files to work then, which would maybe be possible?
-            break;
+                fun2.push((end2 - start2));
+                break;
+            default:
+                //threaded
+                //web worker approach? needs separate files to work then, which would maybe be possible?
+                break;
+        }
     }
+
+    var logText = "faaster_" + experimentID + ": mode" + mode + " ";
+
+    logText += "f1 ";
+    fun1.forEach(function (value, i) {
+        logText += value + " ";
+    });
+
+    logText += "f2 ";
+    fun2.forEach(function (value, i) {
+        logText += value + " ";
+    });
+
+    console.log(logText);
+
     res.send('Ran mode ' + mode + ' according to passed variable');
 })
