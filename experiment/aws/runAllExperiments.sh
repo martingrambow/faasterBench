9735c1e487ad1d788ed20f7969b063bbc7c20eb2
Martin Grambow <grambow@tu-berlin.de>
2023-09-07 14:22:09 +0200

    only consider 0% and 5% regression

--- experiment/aws/runAllExperiments.sh
+++ experiment/aws/runAllExperiments.sh
@@ -13,11 +13,3 @@
 ./runExperiment.sh 5000000 5250000 5 10 5 3
 ./runExperiment.sh 5000000 5250000 5 10 10 3
 ./runExperiment.sh 5000000 5250000 5 10 25 3
-
-
-./runExperiment.sh 5000000 5500000 10 5 5 3
-./runExperiment.sh 5000000 5500000 10 5 10 3
-./runExperiment.sh 5000000 5500000 10 5 25 3
-./runExperiment.sh 5000000 5500000 10 10 5 3
-./runExperiment.sh 5000000 5500000 10 10 10 3
-./runExperiment.sh 5000000 5500000 10 10 25 3
